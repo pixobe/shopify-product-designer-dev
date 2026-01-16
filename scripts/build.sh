a56e5d69365b873314f4bd4963d6915d68fc46d7
pixobe <email@pixobe.com>
2026-01-17 07:13:12 +0800

    Build script update for docker deployment

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,9 @@
 
 docker build -t pixobe.com/shopify-product-designer-dev:$1 .
 APP_VERSION=$1
-sed -i.bak "s/^APP_VERSION=.*/APP_VERSION=$APP_VERSION/" .env && rm -f .env.bak
+
+#update prod
+sed -i.bak "s/^APP_VERSION=.*/APP_VERSION=$APP_VERSION/" .env.prod
+
+#update custom
+sed -i.bak "s/^APP_VERSION=.*/APP_VERSION=$APP_VERSION/" .env.custom
